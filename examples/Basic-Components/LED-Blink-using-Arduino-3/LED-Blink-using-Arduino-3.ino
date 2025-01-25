3664f71fe33b846b664655d5b98b150e564eff96
DUCNGUYEN-creator <huuduc942010@gmail.com>
2025-01-25 12:10:40 +0700

    Create LED-Blink-using-Arduino-3.ino

---
+++ examples/Basic-Components/LED-Blink-using-Arduino-3/LED-Blink-using-Arduino-3.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+const uint8_t LED_PIN = LED_BUILTIN;
+
+int lightOnTime = 2000;
+int lightOffTime = 1000;
+int count = 0;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(lightOnTime);
+  if (count < 3) {
+    count = count + 1;
+    digitalWrite(LED_PIN, LOW);
+    delay(lightOffTime);
+  }
+}
